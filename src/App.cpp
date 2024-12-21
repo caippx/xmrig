1fadc25c8db3f1bcceb1dd888af7cbe2d9739280
caippx <37325838+caippx@users.noreply.github.com>
2024-12-21 22:19:12 +0800

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -1,14 +1,14 @@
 /* XMRig
- * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
- * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
- * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
- * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
- * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
- * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2018      Lee Clagett <https://github.com/vtnerd>
- * Copyright 2018-2024 SChernykh   <https://github.com/SChernykh>
- * Copyright 2016-2024 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
- *
+ * Copyright 2010     
+ * Copyright 2012-2014 
+ * Copyright 2014     
+ * Copyright 2014-2016 
+ * Copyright 2016      
+ * Copyright 2017-2018 
+ * Copyright 2018     
+ * Copyright 2018-2024 
+ * Copyright 2016-2024 
+ * SB weiruan bie feng wo hao
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
@@ -55,7 +55,7 @@ xmrig::App::~App()
 int xmrig::App::exec()
 {
     if (!m_controller->isReady()) {
-        LOG_EMERG("no valid configuration found, try https://xmrig.com/wizard");
+        LOG_EMERG("no valid configuration");
 
         return 2;
     }
@@ -96,7 +96,7 @@ int xmrig::App::exec()
 void xmrig::App::onConsoleCommand(char command)
 {
     if (command == 3) {
-        LOG_WARN("%s " YELLOW("Ctrl+C received, exiting"), Tags::signal());
+        LOG_WARN("%s " YELLOW("tuichu get, exiting"), Tags::signal());
         close();
     }
     else {
